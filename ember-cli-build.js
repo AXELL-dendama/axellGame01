30442e858f7f079f393ac428d68ed923e6db31ab
kengreg <kengreg@axell.tokyo>
2020-06-09 07:33:59 -0400

    allow debug.html to be formatted like index.html by ember-cli

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,12 +1,41 @@
 'use strict';
 
+const path = require('path');
+const Funnel = require('broccoli-funnel');
+const ConfigReplace = require('broccoli-config-replace');
+
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const { configReplacePatterns } = require('ember-cli/lib/utilities/ember-app-utils');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    // do not use <meta> tags
     storeConfigInMeta: false,
+
+    // don't run app automatically, we are lazy loading it into another app
     autoRun: false
+  });
+
+  // add additional debug.html from `app/debug.html` to `dist/debug.html`
+  const debugFilePath = 'debug.html';
+  const pkgr = app._defaultPackager;
+  const debugTree = new Funnel('app', {
+    include: [debugFilePath],
+    allowEmpty: true,
+    annotation: 'Additional HTML file'
+  });
+
+  // replace {{rootURL}} and {{content-for}} in debug.html
+  let patterns = configReplacePatterns({
+    addons: pkgr.project.addons,
+    autoRun: pkgr.autoRun,
+    storeConfigInMeta: pkgr.storeConfigInMeta,
+  });
+
+  const processedDebugTree = new ConfigReplace(debugTree, pkgr.packageConfig(), {
+    configPath: path.join(pkgr.name, 'config', 'environments', `${pkgr.env}.json`),
+    files: [debugFilePath],
+    patterns,
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -22,5 +51,6 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree();
+  // merge `debug.html` tree with ember app
+  return app.toTree(processedDebugTree);
 };
